1e6e748cd96b5d48ce61177e468e31315abdc306
DSUN <dsun@hanmail.net>
2023-08-16 20:18:33 +0900

    2023-08-16 20:18

---
+++ src/Localization/LocalizationManager.cs
@@ -0,0 +1,65 @@
+﻿using System.ComponentModel;
+using System.Globalization;
+
+namespace MetaFrm.Maui.Localization
+{
+    /// <summary>
+    /// LocalizationManager
+    /// </summary>
+    public class LocalizationManager : INotifyPropertyChanged
+    {
+        private LocalizationManager()
+        {
+            if (Preferences.Get("Factory.CultureInfo", null).IsNullOrEmpty())
+                Preferences.Set("Factory.CultureInfo", CultureInfo.CurrentCulture.Name);
+
+            //Factory.CultureInfo = new("en-us");
+            Factory.CultureInfo = new(Preferences.Get("Factory.CultureInfo", null));
+        }
+
+        /// <summary>
+        /// Translate
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public object this[string key]
+        {
+            get
+            {
+                if (key.Contains('^'))
+                {
+                    List<string> tmps = key.Split("^").ToList();
+
+                    key = tmps.FirstOrDefault();
+
+                    tmps.Remove(key);
+
+                    return key.Translate(tmps.ToArray());
+                }
+                else
+                    return key.Translate();
+            }
+        }
+
+        /// <summary>
+        /// Instance
+        /// </summary>
+        public static LocalizationManager Instance { get; } = new();
+
+        /// <summary>
+        /// PropertyChanged
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// SetCulture
+        /// </summary>
+        /// <param name="culture"></param>
+        public void SetCulture(CultureInfo culture)
+        {
+            Factory.CultureInfo = culture;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(null));
+            Preferences.Set("Factory.CultureInfo", Factory.CultureInfo.Name);
+        }
+    }
+}
---
+++ src/Localization/TranslateExtension.cs
@@ -0,0 +1,47 @@
+﻿namespace MetaFrm.Maui.Localization
+{
+    /// <summary>
+    /// TranslateExtension
+    /// </summary>
+    [ContentProperty(nameof(Text)), QueryProperty(nameof(Text), nameof(Args))]
+    public class TranslateExtension : IMarkupExtension<BindingBase>
+    {
+        /// <summary>
+        /// Text
+        /// </summary>
+        public string Text { get; set; }
+
+        /// <summary>
+        /// Args
+        /// </summary>
+        public string Args { get; set; }
+
+        /// <summary>
+        /// ProvideValue
+        /// </summary>
+        /// <param name="serviceProvider"></param>
+        /// <returns></returns>
+        public BindingBase ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Args.IsNullOrEmpty())
+                return new Binding
+                {
+                    Mode = BindingMode.OneWay,
+                    Path = $"[{Text}]",
+                    Source = LocalizationManager.Instance
+                };
+            else
+                return new Binding
+                {
+                    Mode = BindingMode.OneWay,
+                    Path = $"[{Text}^{Args}]",
+                    Source = LocalizationManager.Instance
+                };
+        }
+
+        object IMarkupExtension.ProvideValue(IServiceProvider serviceProvider)
+        {
+            return ProvideValue(serviceProvider);
+        }
+    }
+}
